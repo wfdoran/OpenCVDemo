6c6bb9a757f6159a86b0dd25cadba02774432ea1
William Doran <wfdoran4@yahoo.com>
2026-08-11 21:11:46 -0700

    Code comments in solution13.py.

--- Solutions/solution13.py
+++ Solutions/solution13.py
@@ -1,26 +1,82 @@
+"""Exercise 13
+
+    In this exercise, we are going to create the training data on
+    which to apply some machine learning technique.  The YOLO format
+    gives use patches in the various pictures which contain pollen.
+    We also need negative sample.  To obtain those, we are going to
+    pick 5 random 96 pixel by 96 pixel regions in each picture and
+    check whether it overlaps any of the regions marked as containing
+    pollen.  If it does not overlap any region, we will record it as a
+    negative.
+
+    
+
+"""
+
 import os
 import cv2
 import random
 
 
 def rectangles_overlap(rect_a, rect_b):
-    ax1, ay1, ax2, ay2 = rect_a
-    bx1, by1, bx2, by2 = rect_b
+    """determines if two rectangles overlap.
 
-    return ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1
+        The format of the rectangles records the coordinates of the
+        upper-left and lower-right corner.
+    
+        (upper_left_x, upper_left_y, lower_right_x, lower_right_y)
 
+        One way they might not over lap is A is to the left of the B
+
+             A1 --+
+             |    |
+             |    |         B1 --+
+             +--- A2        |    |
+                            |    |
+                            +---B2
+        
+        So, if A2x <= B1x, the rectangles do not overlap.  There are
+        three other configurations would indicate that rectangles do
+        not overlap.  In order to overlap, none of these can hold.
+
+        Return TRUE is the rectangles overlap.
+    """
+
+    A1x, A1y, A2x, A2y = rect_a
+    B1x, B1y, B2x, B2y = rect_b
+
+    return A1x < B2x and A2x > B1x and A1y < B2y and A2y > B1y
+    
 def any_overlap(boxes, r_box):
+    """ determine r_box overlaps with any of the recrangles in box.
+    """
     for box in boxes:
         if rectangles_overlap(box, r_box):
             return True
     return False
 
 def generate_labelled_data(data_dir, win_size, neg_per_image):
+    """data_dir        directory of the YOLO data
+        win_size        size of the random patches
+        neg_per_image   how many random samples to try
+                        in each image
+
+        Return array
+         [(image_filename, (class, x1, y1, x2, y2))]
+
+        The class is 0 for pollen or -1 for random.
+        (x1, y1, x2, y2) corners of the rectangle.
+
+        We follow exercise12 to get the positive pollen regions.  Then
+        we will generate neg_per_image random regions.  If they do not
+        overlap any of the pollen regions, add them.
+
+    """
     # Set up the paths to the images and labels
     images_dir = os.path.join(data_dir, "images")
     labels_dir = os.path.join(data_dir, "labels")
 
-
+    # training_data[] holds the returned data 
     training_data = []
     for image_file in os.listdir(images_dir):
         # Set up the full path to the image file and the label file.
@@ -37,6 +93,7 @@ def generate_labelled_data(data_dir, win_size, neg_per_image):
         img_width = img.shape[1]
     
         with open(full_label_file) as f:
+            # pollen will contain the pollen rectangles in this image.
             pollen = []
             for line in f:
                 fields = line.split()
@@ -68,17 +125,22 @@ def generate_labelled_data(data_dir, win_size, neg_per_image):
                 pollen.append((x1, y1, x2, y2))
 
             for _ in range(neg_per_image):
+                # Pick a random rectangle
                 x1 = random.randint(0, img_width - win_size[0] - 1)
                 y1 = random.randint(0, img_height - win_size[1] - 1)
                 x2 = x1 + win_size[0]
                 y2 = y1 + win_size[1]
 
+                # If it does not overlap any pollen, record it with
+                # class -1
                 random_box = (x1, y1, x2, y2)
                 if not any_overlap(pollen, random_box):
                     training_data.append((image_file, (-1, x1, y1, x2, y2)))
 
     return training_data
 
+# By having a __main__, we can import solution13.py and use
+# generate_labelled_data in another program.
 if __name__ == "__main__":
     data_dir = "../Pics/pollen_data"
     win_size = (96, 96)
